b0cf752f37b5b7197232a1c282aa3c377020a979
Daniel <dnlnavlap@gmail.com>
2019-04-03 19:59:24 +0200

    Prueba2

--- Proyecto_colaborativo/src/Principal.java
+++ Proyecto_colaborativo/src/Principal.java
@@ -2,7 +2,11 @@ import java.io.*;
 public class Principal {
 
 	public static Serie[] rellenarSeries() {
-		
+		Serie[] arrser = new Serie[5];
+		for (int i = 0; i < 3; i++) {
+			
+		}
+		arrser[3]= new Serie();
 	}
 	
 	public static void main(String[] args) {
